36ffaca288a5bcd09e6210f8855470e3e24e4e97
Husnul Aman <amanh@hsenidmobile.com>
2021-01-24 11:44:22 +0530

    firebase

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -3,14 +3,24 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
 
+// const config = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+//   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+// };
+
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyB-ovvTHyugbMcaOh2m5WmrH41K8cslumQ",
+  authDomain: "event-countdwon.firebaseapp.com",
+  projectId: "event-countdwon",
+  storageBucket: "event-countdwon.appspot.com",
+  messagingSenderId: "58867007839",
+  appId: "1:58867007839:web:ee1a45aebb6e72511836ea",
+  measurementId: "G-PZ9RLVC8DZ",
 };
 
 class Firebase {
